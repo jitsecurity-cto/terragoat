c75f9e561ff5c6822ca0257ae109d3e6fdb60d22
Yonatan Greenfeld <100685375+YonatanGreenfeld@users.noreply.github.com>
2024-08-20 16:01:04 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -142,6 +142,7 @@ resource "aws_s3_bucket" "logs" {
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/s3.tf"
+    access_key           = "AKIAIOSFODNN6EXAMPLE"    
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
